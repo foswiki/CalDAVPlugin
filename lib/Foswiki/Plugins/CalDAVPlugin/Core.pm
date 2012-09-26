a896e19664bac1dc5aae0e3a15ccd24f24c7a5fe
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-09-26 01:37:12 +0000

    Item11808: perltidy, v20120714 which has been installed on foswiki.org enforces a few new rules. Developers, PLEASE UPGRADE! This commit should help
    I've perltidy'ed everything which had a TIDY file.
    Also fixed all errors shown by perltidy (maybe still one encoding issue for Confluence)
    I've fixed everything I saw with obvious perl mistakes though. Doubt anybody
    will care, as most were due to a bogus Build::Contrib::createnewextension which
    Sven fixed recently.
    
    git-svn-id: http://svn.foswiki.org/trunk/CalDAVPlugin@15429 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/CalDAVPlugin/Core.pm
+++ lib/Foswiki/Plugins/CalDAVPlugin/Core.pm
@@ -15,9 +15,10 @@ my %formats = (
     },
     holidaylist => {
         event => '   * $day $month $year - $name - $summary - $icon',
-        range => '   * $day $month $year - $eday $emonth $eyear - $name - $summary',
+        range =>
+          '   * $day $month $year - $eday $emonth $eyear - $name - $summary',
     },
-   );
+);
 
 use constant TRACE => 1;
 
@@ -38,7 +39,7 @@ tokens are supported.
 =cut
 
 sub CALDAV {
-    my($session, $params, $topic, $web) = @_;
+    my ( $session, $params, $topic, $web ) = @_;
 
     my $calendar = $params->{_DEFAULT};
     my $calspec;
@@ -46,12 +47,15 @@ sub CALDAV {
     my $user;
     my $pass;
     if ($calendar) {
+
         # get from setup options
         $calspec = $Foswiki::cfg{Plugins}{CalDAVPlugin}{Calendars}->{$calendar};
         unless ($calspec) {
-            return "<span class='foswikiAlert'>No such calendar '$calendar'</span>";
+            return
+              "<span class='foswikiAlert'>No such calendar '$calendar'</span>";
         }
-    } else {
+    }
+    else {
         $calspec = $params;
     }
     $url  = $calspec->{url};
@@ -61,110 +65,125 @@ sub CALDAV {
     unless ($url) {
         return "<span class='foswikiAlert'>No url given</span>";
     }
-    unless (defined $user && defined $pass) {
+    unless ( defined $user && defined $pass ) {
         return "<span class='foswikiAlert'>user and pass must be given</span>";
     }
 
     my $cal;
-    eval {
-        $cal = Cal::DAV->new( user => $user, pass => $pass, url => $url);
-    };
+    eval { $cal = Cal::DAV->new( user => $user, pass => $pass, url => $url ); };
     unless ($cal) {
         print STDERR "Cal::DAV: $@\n";
         return "<span class='foswikiAlert'>Calendar could not be opened</span>";
     }
 
-    $params->{name} = $calendar unless defined $params->{name};
-    $params->{name} = $params->{url} unless defined $params->{name};
-    $params->{separator} = '$n()' unless defined $params->{separator};
+    $params->{name}      = $calendar      unless defined $params->{name};
+    $params->{name}      = $params->{url} unless defined $params->{name};
+    $params->{separator} = '$n()'         unless defined $params->{separator};
 
     my $stop = $params->{stop};
     $stop = "50,365" unless defined($stop);
-    unless($stop =~ /^(\d+), *(\d+)$/) {
+    unless ( $stop =~ /^(\d+), *(\d+)$/ ) {
         return "<span class='foswikiAlert'>Invalid stop=\"$stop\"</span>";
     }
     my ( $stopCount, $stopDays ) = ( $1, $2 );
 
     my @events;
-    foreach my $e ( @{$cal->entries()}) {
+    foreach my $e ( @{ $cal->entries() } ) {
         next unless $e->ical_entry_type() eq 'VEVENT';
         my %event;
         my $props = $e->properties();
-        while (my ($k, $e) = each %$props) {
+        while ( my ( $k, $e ) = each %$props ) {
             my $p = $e->[0];
-            if ($k eq 'dtstart') {
+            if ( $k eq 'dtstart' ) {
                 $event{dtstart} = $p->{value};
                 $event{start} =
-                  DateTime::Format::ICal->parse_datetime($p->value);
-                push(@events, " dtstart ".$event{start}) if (TRACE);
-            } elsif ($k eq 'dtend') {
+                  DateTime::Format::ICal->parse_datetime( $p->value );
+                push( @events, " dtstart " . $event{start} ) if (TRACE);
+            }
+            elsif ( $k eq 'dtend' ) {
                 $event{end} =
-                  DateTime::Format::ICal->parse_datetime($p->value);
-                push(@events, " dtend ".$event{end}) if (TRACE);
-            } elsif ($k eq 'summary') {
+                  DateTime::Format::ICal->parse_datetime( $p->value );
+                push( @events, " dtend " . $event{end} ) if (TRACE);
+            }
+            elsif ( $k eq 'summary' ) {
                 $event{summary} = $p->{value};
-                push(@events, " summary ".$event{summary}) if (TRACE);
-            } elsif ($k eq 'description') {
+                push( @events, " summary " . $event{summary} ) if (TRACE);
+            }
+            elsif ( $k eq 'description' ) {
                 $event{description} = $p->{value};
-            } elsif ($k eq 'rrule') {
-                push(@events, " rrule $p->{value}") if (TRACE);
+            }
+            elsif ( $k eq 'rrule' ) {
+                push( @events, " rrule $p->{value}" ) if (TRACE);
                 $event{recurrence} = $p->{value};
             }
         }
-        if ($event{recurrence}) {
+        if ( $event{recurrence} ) {
+
             # count= is broken in DateTime::Event::ICal
-            if ($event{recurrence} =~ s/;?count=(\d+);?/;/i) {
+            if ( $event{recurrence} =~ s/;?count=(\d+);?/;/i ) {
                 $event{count} = $1;
             }
             my $recur = DateTime::Format::ICal->parse_recurrence(
                 recurrence => $event{recurrence},
-                dtstart => $event{start});
+                dtstart    => $event{start}
+            );
+
             # Limit to the request range *or* $stopDays *or*
             # $stopCount repeats
             my $span = DateTime::Span->from_datetimes(
-                start => $event{start} ||
-                  DateTime->from_epoch(epoch => time),
+                start => $event{start} || DateTime->from_epoch( epoch => time ),
                 end => DateTime->from_epoch(
-                    epoch => time+60*60*24*$stopDays ));
+                    epoch => time + 60 * 60 * 24 * $stopDays
+                )
+            );
             $event{count} ||= $stopCount;
-            $event{count} = $stopCount if ($event{count} > $stopCount);
+            $event{count} = $stopCount if ( $event{count} > $stopCount );
 
-            my $s = Foswiki::Time::parseTime($event{start});
-            push(@events,
-                 {
-                     start => $s,
-                     summary => $event{summary},
-                     description => $event{description},
-                 });
+            my $s = Foswiki::Time::parseTime( $event{start} );
+            push(
+                @events,
+                {
+                    start       => $s,
+                    summary     => $event{summary},
+                    description => $event{description},
+                }
+            );
             $event{count}--;
-            my $iter = $recur->iterator(span => $span);
-            while ( $event{count} && (my $dt = $iter->next) ) {
+            my $iter = $recur->iterator( span => $span );
+
+            while ( $event{count} && ( my $dt = $iter->next ) ) {
                 my $d = Foswiki::Time::parseTime($dt);
                 next if $d == $s;
-                push(@events,
-                     {
-                         start => $d,
-                         summary => $event{summary},
-                         description => $event{description},
-                     });
+                push(
+                    @events,
+                    {
+                        start       => $d,
+                        summary     => $event{summary},
+                        description => $event{description},
+                    }
+                );
                 $event{count}--;
             }
-        } else {
-            push(@events,
-                 {
-                     start => Foswiki::Time::parseTime($event{start}),
-                     end => (defined $event{end}) ?
-                       Foswiki::Time::parseTime($event{end}) : undef,
-                     summary => $event{summary},
-                     description => $event{description},
-                 });
+        }
+        else {
+            push(
+                @events,
+                {
+                    start => Foswiki::Time::parseTime( $event{start} ),
+                    end   => ( defined $event{end} )
+                    ? Foswiki::Time::parseTime( $event{end} )
+                    : undef,
+                    summary     => $event{summary},
+                    description => $event{description},
+                }
+            );
         }
     }
-    return _formatEvents(\@events, $params);
+    return _formatEvents( \@events, $params );
 }
 
 sub _formatEvents {
-    my ($events, $params) = @_;
+    my ( $events, $params ) = @_;
 
     my $format;
     my $target = $params->{target} || 'calendar';
@@ -175,24 +194,25 @@ sub _formatEvents {
     $es = $format->{event} unless defined $es;
     my $rs = $params->{range};
     $rs = $format->{range} unless defined $rs;
-    my @r = map { _formatEvent($_, $params, $es, $rs) } @$events;
-    return Foswiki::Func::decodeFormatTokens(join($params->{separator}, @r));
+    my @r = map { _formatEvent( $_, $params, $es, $rs ) } @$events;
+    return Foswiki::Func::decodeFormatTokens(
+        join( $params->{separator}, @r ) );
 }
 
 sub _formatEvent {
-    my ($event, $params, $es, $rs) = @_;
+    my ( $event, $params, $es, $rs ) = @_;
     return $event unless ref($event);
-    my $s = (defined $event->{end}) ? $rs : $es;
-    $s = Foswiki::Time::formatTime($event->{start}, $s);
-    if (defined $event->{end}) {
+    my $s = ( defined $event->{end} ) ? $rs : $es;
+    $s = Foswiki::Time::formatTime( $event->{start}, $s );
+    if ( defined $event->{end} ) {
         $s =~ s/\$e(seco?n?d?s?|minu?t?e?s?|hour?s?|day|w(eek|day)|dow
                 |mo(?:nt?h?)?|ye(?:ar)?)(\(\)|(?=\W|$))/\$$1/gx;
-        $s = Foswiki::Time::formatTime($event->{end}, $s);
+        $s = Foswiki::Time::formatTime( $event->{end}, $s );
     }
-    foreach my $f (keys %$event) {
+    foreach my $f ( keys %$event ) {
         $s =~ s/\$$f(\(\)|(?=\W|$))/$event->{$f}/;
     }
-    foreach my $f (keys %$params) {
+    foreach my $f ( keys %$params ) {
         next if $f =~ /^_/;
         $s =~ s/\$$f(\(\)|(?=\W|$))/$params->{$f}/;
     }
